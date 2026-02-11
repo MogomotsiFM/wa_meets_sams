4c391f77bda83f8edc1404f0791fe8fa16c85ca7
Mogomotsi <amg.seiphemo@gmail.com>
2026-02-11 16:54:57 +0200

    Cached the combo boxes for fast access.

--- print_pdf_reports.py
+++ print_pdf_reports.py
@@ -14,7 +14,7 @@ from pywinauto.application import Application
 
 from typing import Literal
 
-def select_combo_box_option(window, combo_box, value):
+def select_combo_box_option(combo_box: ComboBoxWrapper, value: str):
 	"""
 	There seems to be a bug with how pywinauto::ComboBox::select works.
 	
@@ -23,22 +23,12 @@ def select_combo_box_option(window, combo_box, value):
 	:param value: The option in the combo box that we want to select
 	"""
 
-	grade_window = window.window(parent=window, best_match=combo_box, control_type="ComboBox")
-	print("Class: ", grade_window.class_name())
-	#grade_window.window(control_type="Button").click()
-	grade_window = grade_window.expand()
-	kids = grade_window.children(control_type="List")
-	# Flatten the list of lists
-	# Strip spaces because "All" is written " All"
-	ks = [k for ks in kids[0].texts() for k in ks]
-	if len(ks) == 0:
-		print("Number of ComboBox: ", len(kids))
-		print([k for kid in kids for ks in kid for k in ks])
-	print("Children: ", ks)
+	combo_box = combo_box.expand()
+	kids = combo_box.children(control_type="List")
 	kids[0].item(value).invoke()
 
 
-def get_combo_box_options(window, combo_box):
+def get_combo_box_options(options_combo: ComboBoxWrapper) -> list[str]:
 	"""
 	Returns the list of options available from the combo box.
 	These options are then forwarded to the GUI so that the user may select one of them.
@@ -47,18 +37,21 @@ def get_combo_box_options(window, combo_box):
 	:param combo_box: The name of the combo box of interest
 	"""
 	
-	options_combo = window.window(parent=window, best_match=combo_box, control_type="ComboBox")
 	options_combo = options_combo.expand()
 	kids = options_combo.children(control_type="List")
 	labels = [k for ks in kids[0].texts() for k in ks]
-	if len(labels) == 0:
-		print("Number of ComboBox: ", len(kids))
-		print([k for kid in kids for ks in kid for k in ks])
 	options_combo.collapse()
 	return labels
 
 
-def process_learner(window, grade, room, report_format, report_path):
+def get_combo_boxes(window):
+	cbs = window.descendants(control_type="ComboBox")
+	id_combo_box_map = {cb.control_id(): cb for cb in cbs}
+	
+	return id_combo_box_map
+
+
+def process_learner(window, grade, room, report_format, report_file_path, report_format_combo_box: ComboBoxWrapper):
 	"""
 	Prints the report of one learner to file
 
@@ -69,23 +62,21 @@ def process_learner(window, grade, room, report_format, report_path):
 	window.window(best_match="Learners", control_type="Group").window(best_match="All", control_type="Button").click()
 
 	# Select report format
-	report_format_window = window.window(best_match="Select progress report format", control_type="Group")
-	select_combo_box_option(report_format_window, None, report_format)
+	select_combo_box_option(report_format_combo_box, report_format)
 	
 	window.window(best_match="Language to print", control_type="Group").English.click()
 
 	window.window(best_match="Select filter options", control_type="Group").window(best_match="Selected learner", control_type="RadioButton").click()
 
 	filename = f"{grade}_{room}"
-	filename = os.path.join(report_path, filename)
+	filename = os.path.join(report_file_path, filename)
 	button = window.window(best_match="Print progress report", control_type="Button")
 
 	print_pdf(button, filename)
 
 
-def print_cover_page(phase: Literal["FET", "Senior"], report_path):
-	parent = window.window(best_match="Blank report covers", control_type="Group")
-	select_combo_box_option(parent, None, phase)
+def print_cover_page(combo_box: ComboBoxWrapper, phase: Literal["FET", "Senior"], report_path: str):
+	select_combo_box_option(combo_box, phase)
 
 	filename = f"{phase}_report_cover"
 	filename = os.path.join(report_path, filename)
@@ -209,47 +200,67 @@ date = date.replace(":", "T")
 path = os.path.join(path, report_folder, date)
 os.makedirs(name=path, exist_ok=True)
 
+#window.print_control_identifiers()
+id_cb_map = get_combo_boxes(window)
+
+#--------------------------------- START OF REPORT PRINTING PROCESS FOR A ROOM -----------------------------------------------
+
 classes_window = window.window(parent=window, best_match="Select Options")
-classes_window.ComboBox2.draw_outline()
-print("Year: ", get_combo_box_options(classes_window, "Combobox2"))
-select_combo_box_option(classes_window, "ComboBox2", year)
+year_combo_box = id_cb_map[73]
+year_combo_box.draw_outline()
+print("Year: ", get_combo_box_options(year_combo_box))
+select_combo_box_option(year_combo_box, value=year)
 
-classes_window.ComboBox3.draw_outline()
-print("Grades: ", get_combo_box_options(classes_window, "Combobox3"))
-select_combo_box_option(classes_window, "ComboBox3", grade)
+#classes_window.ComboBox3.draw_outline()
+#classes_window.window(control_id=72, control_type="ComboBox").draw_outline()
+#print("Grades: ", get_combo_box_options(classes_window, control_id=72))
+#select_combo_box_option(classes_window, control_id=72, value=grade)
+grade_combo_box = id_cb_map[74]
+grade_combo_box.draw_outline()
+print("Year: ", get_combo_box_options(grade_combo_box))
+select_combo_box_option(grade_combo_box, value=grade)
 
-classes_window.ComboBox4.draw_outline()
-print("Classes: ", get_combo_box_options(classes_window, "ComboBox4"))
-select_combo_box_option(classes_window, "ComboBox4", room)
+#classes_window.ComboBox4.draw_outline()
+#classes_window.window(control_id=73, control_type="ComboBox").draw_outline()
+#print("Classes: ", get_combo_box_options(classes_window, control_id=73))
+#select_combo_box_option(classes_window, control_id=73, value=room)
+room_combo_box = id_cb_map[75]
+room_combo_box.draw_outline()
+print("Year: ", get_combo_box_options(room_combo_box))
+select_combo_box_option(room_combo_box, value=room)
 
-classes_window.ComboBox6.draw_outline()
-print("Cycle: ", get_combo_box_options(classes_window, "ComboBox6"))
-select_combo_box_option(classes_window, "ComboBox6", cycle)
+#classes_window.ComboBox6.draw_outline()
+#classes_window.window(control_id=75, control_type="ComboBox").draw_outline()
+#print("Cycle: ", get_combo_box_options(classes_window, control_id=75))
+#select_combo_box_option(classes_window, control_id=75, value=cycle)
+cycle_combo_box = id_cb_map[72]
+cycle_combo_box.draw_outline()
+print("Year: ", get_combo_box_options(cycle_combo_box))
+select_combo_box_option(cycle_combo_box, value=cycle)
 
 # There might be a bug in ComboBox.texts()
-[print(cl, cl.draw_outline(), cl.selected_text()) for cl in window.window(best_match="Select Options").children(control_type="ComboBox")]
+#[print(cl, cl.draw_outline(), cl.selected_text()) for cl in window.window(best_match="Select Options").children(control_type="ComboBox")]
+[print(cl, cl.draw_outline(), cl.selected_text()) for _, cl in id_cb_map.items()]
 
 window.GO.click()
 
 # Configure report format
 # Do we need different format for different phases: FET vs senior?
-report_format_window = window.window(best_match="Select progress report format", control_type="Group")
-report_format_window.window(control_type="ComboBox").draw_outline()
 # Display these on the GUI
-formats = get_combo_box_options(report_format_window, None)
+format_combo_box = id_cb_map[40]
+format_combo_box.draw_outline()
+formats = get_combo_box_options(format_combo_box)
 print("Report formats: ", formats)
-select_combo_box_option(report_format_window, None, format)
-
-#window.print_control_identifiers()
+select_combo_box_option(format_combo_box, value=format)
 
 # Print one PDF with reports for all the learners in a class(grade + room).
 progress_report_window = window.window(best_match="Print progress reports", control_type="Window")
-process_learner(progress_report_window, grade, room, format, path)
+process_learner(progress_report_window, grade, room, format, path, format_combo_box)
 
-print_cover_page("FET", path)
-print_cover_page("Senior", path)
+#---------------------------------- END OF REPORT PRINTING PROCESS FOR A ROOM ---------------------------------------------------
 
-#----------------------- END OF REPORT PRINTING PROCESS -----------------------------------------
+print_cover_page(id_cb_map[60], "FET", path)
+print_cover_page(id_cb_map[60], "Senior", path)
 
 window.window(best_match="Print progress reports", control_type="Window").Done.click()
 
