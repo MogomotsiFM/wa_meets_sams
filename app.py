6ab9109fc1868d898b4fc77ed0ffced413e30c1e
Mogomotsi <amg.seiphemo@gmail.com>
2026-02-22 21:35:01 +0200

    Moved the creation of output directories outside the Presenter.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import sys
 import logging
 
+from datetime import datetime
+
 sys.coinit_flags = 2
 
 from PyQt5.QtWidgets import QApplication
@@ -27,7 +29,18 @@ logging.basicConfig(
 
 sams_path = os.path.join("C:\\", "Users", "GAME", "Desktop", "EdusolSAMS")
 
-presenter = Presenter(sams_path)
+# Set up directories for reports and cover pages as the current directory of the app.
+# The current directory
+reports_path = os.path.dirname(os.path.abspath(__file__))
+
+date = f"{datetime.now()}"
+date = date.replace(":", "T")
+cover_pg_path = os.path.join(reports_path, "Reports", date, "covers")
+report_path = os.path.join(reports_path, "Reports", date, "reports")
+os.makedirs(name=cover_pg_path, exist_ok=True)
+os.makedirs(name=report_path, exist_ok=True)
+
+presenter = Presenter(sams_path, cover_pg_path, report_path)
 printer = ReportPrinter(presenter)
 
 app = QApplication(sys.argv)
