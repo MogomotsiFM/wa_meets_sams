7f842e75db7691e8027f6ec5750103da217224f3
Mogomotsi <amg.seiphemo@gmail.com>
2026-03-09 23:23:09 +0200

    Added a number of TODOs.

--- Processes/process_incoming_messages.py
+++ Processes/process_incoming_messages.py
@@ -22,7 +22,17 @@ load_dotenv()
 
 STOPWORD = "STOP"
 
-# TODO: How to handle parents with multiple children. We do not need to send multiple opt-in messages to the same number.
+# TODO: How to handle parents with multiple children. We do not need to send 
+#       multiple opt-in messages to the same number.
+# TODO: If a guardian opts to fetch the report from school then we need to add 
+#       that report to the dead letter queue. This means that we have to decrypt 
+#       that report.
+# TODO: It is also possible that a phone number is not on WhatsApp. Again, we must 
+#       add the corresponding report to the dead letter queue.
+# TODO: Join all the reports in the dead letter queue for easy printing. We may 
+#       also want to remove the report cover pages.
+# TODO: Generate some sort of report at the end. How many reports were sent via
+#       WA? How many per grade? How many still need to be collected?
 
 logger = logging.getLogger()
 
