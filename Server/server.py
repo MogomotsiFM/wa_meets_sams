ea4da10ee65bed0ed42463bbeaee94a1f17eec9e
Mogomotsi <amg.seiphemo@gmail.com>
2026-03-12 22:47:42 +0200

    Moved the port number and localtunnel subdomain name to environment variables.
    Moved test-app related code from the server to the test app :-|

--- Server/server.py
+++ Server/server.py
@@ -48,12 +48,12 @@ if not VERIFY_TOKEN or not APP_SECRET:
 # Redis channel to store responses to opt-in messages
 OPT_IN_RESPONSES = os.getenv("OPT_IN_RESPONSES")
 
-APP_PORT = 4001
-subdomain = "mogomotsihs"
+APP_PORT = os.getenv("PORT")
+lt_subdomain = os.getenv("LOCAL_TUNNEL_SUB_DOMAIN")
 # This is beautiful code
 @asynccontextmanager
 async def lifespan_local_tunnel(app: FastAPI):
-    status, tunnel_url, tunnel_process = tunnel.create_tunnel(port=APP_PORT, subdomain=subdomain)
+    status, tunnel_url, tunnel_process = tunnel.create_tunnel(port=APP_PORT, subdomain=lt_subdomain)
     if status:
         print(f"Tunnel created successfully! URL: {tunnel_url}")
     else:
@@ -189,38 +189,4 @@ async def webhook_receive(request: Request):
 @app.get("/")
 async def root(request: Request):
     return PlainTextResponse("WhatsApp SAMS Server is running.")
-
-
-async def upload_files():
-    from redis.asyncio import Redis
-    r = await Redis.from_url("redis://localhost")
-
-    await asyncio.sleep(45)
-
-    img_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\school_emblem.png"
-    await r.publish("pending_delivery_filenames", img_path)
-    
-    await asyncio.sleep(2)
-
-    #file_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\Mogomotsi KEAIKITSE - Tel0731948818 - EMailamg.seiphemo@gmail.com.pdf"
-    file_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\Mogomotsi KEAIKITSE - Tel0710491875Tel0731948818 - EMailamg.seiphemo@gmail.com.pdf"
-    await r.publish("pending_delivery_filenames", file_path)
-
-    await asyncio.sleep(2)
-    
-    file_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\Segomotsi KEAIKITSE - Tel27731948818 - EMailamg.seiphemo@gmail.com.pdf"
-    await r.publish("pending_delivery_filenames", file_path)
-    
-
-async def run_helper(port):
-    uvi = lambda: uvicorn.run("Server.server:app", port=APP_PORT)
-    await asyncio.gather(
-        pim.process_messages(),
-        asyncio.to_thread(uvi),
-        upload_files()
-    )
-
-
-def run(port):
-    asyncio.run(run_helper(port))
 
--- app2.py
+++ app2.py
@@ -1,8 +1,47 @@
+import asyncio
+import uvicorn
+
 from Server.server import run
 from Server import server
 
+from Processes import process_incoming_messages as pim
+
 import dotenv
 dotenv.load_dotenv()
+
+
+async def upload_files():
+    from redis.asyncio import Redis
+    r = await Redis.from_url("redis://localhost")
+
+    await asyncio.sleep(45)
+
+    img_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\school_emblem.png"
+    await r.publish("pending_delivery_filenames", img_path)
+    
+    await asyncio.sleep(2)
+
+    #file_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\Mogomotsi KEAIKITSE - Tel0731948818 - EMailamg.seiphemo@gmail.com.pdf"
+    file_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\Mogomotsi KEAIKITSE - Tel0710491875Tel0731948818 - EMailamg.seiphemo@gmail.com.pdf"
+    await r.publish("pending_delivery_filenames", file_path)
+
+    await asyncio.sleep(2)
+    
+    file_path = r"C:\Users\GAME\Desktop\Projects\whatsapp_sams\Data\Segomotsi KEAIKITSE - Tel27731948818 - EMailamg.seiphemo@gmail.com.pdf"
+    await r.publish("pending_delivery_filenames", file_path)
+    
+
+async def run_helper(port):
+    uvi = lambda: uvicorn.run("Server.server:app", port=port)
+    await asyncio.gather(
+        pim.process_messages(),
+        asyncio.to_thread(uvi),
+        upload_files()
+    )
+
+
+def run(port):
+    asyncio.run(run_helper(port))
 
 if __name__ == "__main__":
     # Run redis on a docker instance
